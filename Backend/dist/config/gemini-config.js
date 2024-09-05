24e48aa917b611ac7423ace50ec1dc0a073dc4b2
Neer Jain <jneer47@gmail.com>
2024-09-05 20:39:50 +0530

    Add .gitignore and remove unnecessary files from tracking

--- Backend/dist/config/gemini-config.js
+++
@@ -1,7 +0,0 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-export const configureGemini = () => {
-    const apiKey = process.env.API_KEY;
-    const genAI = new GoogleGenerativeAI(apiKey);
-    return genAI;
-};
-//# sourceMappingURL=gemini-config.js.map
